246d35e12d9d01d1e629163ba064fc965840ee22
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-01 00:39:19 +0000

    Add picture logo link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { NavLink, useLocation } from "react-router-dom"
+import '../theme.css';
+import React from 'react';
+import { NavLink, useLocation } from "react-router-dom";
 
 function Header() {
   let currentRoute = useLocation().pathname
   let currentlyOnPortal = currentRoute.includes("manage")
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <NavLink to='/' className="navbar-brand">Senpai Engineering</NavLink>
+            <NavLink to='/' className="navbar-brand"><img className="logo-img" src="https://drive.google.com/uc?export=download&id=17SixfxmepKF7HtHlO1BVCnWkoqiFNYYk" alt="HomePage" /></NavLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,48 +15,48 @@ export default function Home(){
             </div>
             {/* <!-- Content Row--> */}
             <div className="row gx-4 gx-lg-5">
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img class="img mx-auto" src="https://img.icons8.com/material-rounded/90/3e54ac/money-bag.png" alt="Multiple trading pairs placeholder" />
                         <div className="text-center">
                             <p className="card-text">Multiple trading pairs</p>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img class="img mx-auto" src="https://img.icons8.com/ios-glyphs/90/3e54ac/key--v1.png" alt="Secure placeholder" />
                         <div className="text-center">
                             <p className="card-text">Secure</p>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img class="img mx-auto" src="https://img.icons8.com/ios-glyphs/90/3e54ac/handshake--v1.png" alt="Investment ready placeholder" />
                         <div className="text-center">
                             <p className="card-text">Investment ready</p>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img class="img mx-auto" src="https://img.icons8.com/ios-glyphs/90/3e54ac/hand-cursor.png" alt="MPESA placeholder" />
                         <div className="text-center">
                             <p className="card-text">MPESA</p>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img class="img mx-auto" src="https://img.icons8.com/ios-filled/90/3e54ac/upload-to-cloud--v1.png" alt="Data backups placeholder" />
                         <div className="text-center">
                             <p className="card-text">Data backups</p>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 mb-5">
-                    <div className="card h-100 py-4">
+                <div className="col-md-4 my-3">
+                    <div className="card shadow-sm h-100 py-4">
                         <img className="img mx-auto" src="https://img.icons8.com/ios-filled/90/3e54ac/fingerprint.png" alt="Privacy placeholder" />
                         <div className="text-center">
                             <p className="card-text">Privacy</p>
