abd7354e0ddcfff6fb65b5b45ca91d3192f27d64
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-02 23:56:27 +0000

    implement frontend login state management

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,53 @@
 import '../theme.css';
-import React from 'react';
-import { NavLink, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { NavLink } from "react-router-dom";
+// eslint-disable-next-line
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  let currentRoute = useLocation().pathname
-  let currentlyOnPortal = currentRoute.includes("manage")
+  const [loggedIn, setLoggedIn] = useState(undefined);
+  // eslint-disable-next-line
+  const navigate = useNavigate();
+
+  function getLoginStatus(){
+    fetch("/login")
+    .then(res => {
+      if (res.ok) { console.log("login ok"); setLoggedIn(true)} 
+      else if (res['status'] === 401){ setLoggedIn(false)}
+      throw new Error('Login status error');
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+
+  const handleLogout = async() => {
+    let response = undefined
+    try {
+      response = await fetch("/login", { method: "DELETE"});
+    } catch (error){
+      console.log("errors")
+    }
+
+    if (response?.ok) {
+      navigate("/login");
+    }
+  }
+
+  useEffect(() => getLoginStatus(), []);
+
   return (
     <nav class="navbar navbar-expand-lg header-footer-theme">
         <div class="container-fluid">
             <NavLink to='/' className="navbar-brand"><img className="logo-img" src="https://drive.google.com/uc?export=download&id=17SixfxmepKF7HtHlO1BVCnWkoqiFNYYk" alt="HomePage" /></NavLink>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 theme-font text-white">
-                  <li class="nav-item">{currentlyOnPortal?<NavLink to='/manage/personnel' className="nav-link">Manage-personnel</NavLink> : <NavLink to='/' className="nav-link">Home</NavLink>}</li>
-                  <li class="nav-item">{currentlyOnPortal?<NavLink to='/manage/jobs' className="nav-link">Manage-jobs</NavLink> : <NavLink to='/login' className="nav-link">Staff-login</NavLink>}</li>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>            
+            {(loggedIn !== undefined)&&(
+              <div clasNames="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 theme-font text-white">
+                  <li className="nav-item">{loggedIn?<button className="btn btn-link" onClick={handleLogout}>Logout</button> : <NavLink to='/login' className="nav-link">Login</NavLink>}</li>
                 </ul>
-            </div>
+              </div>
+            )}
         </div>
     </nav>
   )
