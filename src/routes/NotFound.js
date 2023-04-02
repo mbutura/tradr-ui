905315736fb3d0f9e5bdf46661d6103e57591648
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-02 20:58:25 +0000

    modify not found page to be theme-aligned

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,6 +1,7 @@
 import React, { useEffect, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const NotFound = () => {
   let navigate = useNavigate();
@@ -8,24 +9,25 @@ const NotFound = () => {
   useEffect(() => {
     const redirectHome = setTimeout(() => {
       navigate(-1);
-    }, 5000);
+    }, 8000);
     return () => clearTimeout(redirectHome);
   }, [navigate]);
 
   return (
     <>
       <Header />
-      <div className="container my-5">
+      <div className="container-fluid py-5 extend-height login-background">
         <div className="row justify-content-center">
           <div className="col-lg-6">
             <div className="jumbotron text-center">
-              <h1 className="display-3">404 - Page Not Found</h1>
-              <p className="lead">Sorry, the page you are looking for does not exist.</p>
-              <p>Redirecting to the previous page in 5 seconds...</p>
+              <h1 className="display-3 theme-font">404 - Page Not Found</h1>
+              <p className="lead theme-font">Sorry, the page you are looking for does not exist.</p>
+              <p>Redirecting to the previous page in 8 seconds...</p>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
